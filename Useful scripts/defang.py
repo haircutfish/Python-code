a428717667b232ebf3bfcf50b90c7eaa7a5de9cc
HaircutFish <68355453+haircutfish@users.noreply.github.com>
2024-06-18 02:13:45 -0700

    Update defang.py

--- Useful scripts/defang.py
+++ Useful scripts/defang.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python3
+
 #Simple Python script used to defanging IP and Domain addresses
 #Created by Dan R. 2024-01-15
+#Updated 2024-06-18
+
+import sys
+import os
 
 def addressDefanger(address):
     defang = address.replace('.', '[.]')
     return defang
 
-rawAddress = input('Please enter the Domain or IP you would like to defang: ')
+if len(sys.argv) < 2:
+    print('Please specify the file you want to defang the IPs/Domains')
+    exit(1)
 
-defanged_address = addressDefanger(rawAddress)
+addressFile = sys.argv[1]
+
+with open(addressFile, 'r') as file:
+    lines = file.readlines()
+
+for ip in lines:
+    ip.strip()
+    defanged_address = addressDefanger(ip)
 
 print(defanged_address)
